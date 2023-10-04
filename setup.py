9f4b70c95ec46e1b319ba66ac7fb05268bc7e8fa
Elias Bonnici <elias.bonnici@yubico.com>
2023-10-04 15:24:41 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+"""
+Setup script for `bark_core`
+"""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bark-core',
+    version='0.0.1',
+    packages=find_packages(),
+)
